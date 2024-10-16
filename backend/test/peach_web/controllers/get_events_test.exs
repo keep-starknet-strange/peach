4c7337af7b5c791e6e4647077b968dd56b4e32e4
LucasLvy <lucaslevy10@gmail.com>
2024-10-16 15:17:40 +0200

    fix coverage

--- backend/test/peach_web/controllers/get_events_test.exs
+++ backend/test/peach_web/controllers/get_events_test.exs
@@ -29,30 +29,6 @@ defmodule PeachWeb.GetEventControllerTest do
     {:ok, event1: event1, event2: event2, event3: event3}
   end
 
-  test "returns events after specified time and id", %{conn: conn, event2: event2, event3: event3} do
-    after_datetime = "2024-11-08T00:00:00"
-    after_event_id = "#{event2.id}"
-    first = "10"
-
-    conn =
-      get(
-        conn,
-        "/api/events?after_datetime=#{after_datetime}&after_event_id=#{after_event_id}&first=#{first}"
-      )
-
-    assert json_response(conn, 200)["events"] == [
-             %{
-               "id" => event3.id,
-               "name" => event3.name,
-               "description" => event3.description,
-               "start" => NaiveDateTime.to_iso8601(event3.start),
-               "end" => NaiveDateTime.to_iso8601(event3.end),
-               "location" => event3.location,
-               "cover" => event3.cover
-             }
-           ]
-  end
-
   test "returns events without optional params", %{conn: conn} do
     conn = get(conn, "/api/events")
     response = json_response(conn, 422)
@@ -61,7 +37,30 @@ defmodule PeachWeb.GetEventControllerTest do
 
   test "returns validation error for invalid after_datetime", %{conn: conn} do
     after_datetime = "invalid-time-format"
+    after_event_id = 1
+    first = 10
     conn = get(conn, "/api/events?after_datetime=#{after_datetime}")
+
+    response = json_response(conn, 422)
+    assert response["errors"]["after_datetime"] == "invalid_format"
+
+    conn =
+      get(conn, "/api/events?after_datetime=#{after_datetime}&after_event_id=#{after_event_id}")
+
+    response = json_response(conn, 422)
+    assert response["errors"]["after_datetime"] == "invalid_format"
+
+    conn =
+      get(conn, "/api/events?after_datetime=#{after_datetime}&first=#{first}")
+
+    response = json_response(conn, 422)
+    assert response["errors"]["after_datetime"] == "invalid_format"
+
+    conn =
+      get(
+        conn,
+        "/api/events?after_datetime=#{after_datetime}&after_event_id=#{after_event_id}&first=#{first}"
+      )
 
     response = json_response(conn, 422)
     assert response["errors"]["after_datetime"] == "invalid_format"
@@ -125,5 +124,50 @@ defmodule PeachWeb.GetEventControllerTest do
                "cover" => event2.cover
              }
            ]
+  end
+
+  test "returns events after specified time and id", %{conn: conn, event2: event2, event3: event3} do
+    after_datetime = "2024-11-08T00:00:00"
+    after_event_id = "#{event2.id}"
+    first = "10"
+
+    expected_event =
+      [
+        %{
+          "id" => event3.id,
+          "name" => event3.name,
+          "description" => event3.description,
+          "start" => NaiveDateTime.to_iso8601(event3.start),
+          "end" => NaiveDateTime.to_iso8601(event3.end),
+          "location" => event3.location,
+          "cover" => event3.cover
+        }
+      ]
+
+    conn =
+      get(
+        conn,
+        "/api/events?after_datetime=#{after_datetime}&after_event_id=#{after_event_id}&first=#{first}"
+      )
+
+    assert json_response(conn, 200)["events"] == expected_event
+
+    conn =
+      get(
+        conn,
+        "/api/events?after_datetime=#{after_datetime}&after_event_id=#{after_event_id}"
+      )
+
+    assert json_response(conn, 200)["events"] == expected_event
+
+    after_datetime = "2024-11-16T00:00:00"
+
+    conn =
+      get(
+        conn,
+        "/api/events?after_datetime=#{after_datetime}"
+      )
+
+    assert json_response(conn, 200)["events"] == expected_event
   end
 end
