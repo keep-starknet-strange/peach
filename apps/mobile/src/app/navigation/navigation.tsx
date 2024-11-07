d603202c4de5a2d1ce089dfffa64e57db5af2901
0xChqrles <clanier.dev@gmail.com>
2024-11-07 14:14:57 +0100

    format

--- apps/mobile/src/app/navigation/navigation.tsx
+++ apps/mobile/src/app/navigation/navigation.tsx
@@ -1,62 +1,64 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MobileScreens } from 'peach/src/types/screens/mobile'
-import { AppStackParamList, TabStackParamList } from 'src/app/navigation/types'
-import React, { useCallback } from 'react'
-import { FeedScreen } from 'src/screens/FeedScreen'
-import { ExploreScreen } from 'src/screens/ExploreScreen'
-import { MyTicketsScreen } from 'src/screens/MyTicketsScreen'
 import { BlurView } from '@react-native-community/blur'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { MobileScreens } from 'peach/src/types/screens/mobile'
+import React, { useCallback } from 'react'
 import { StyleSheet } from 'react-native'
+import { AppStackParamList, TabStackParamList } from 'src/app/navigation/types'
 import { TabIcon, TabIconProps } from 'src/components/layout/TabHelpers'
+import { ExploreScreen } from 'src/screens/ExploreScreen'
+import { FeedScreen } from 'src/screens/FeedScreen'
+import { MyTicketsScreen } from 'src/screens/MyTicketsScreen'
 import { useSporeColors } from 'ui/src'
 
 const AppStack = createNativeStackNavigator<AppStackParamList>()
-const TabStack = createBottomTabNavigator<TabStackParamList>();
+const TabStack = createBottomTabNavigator<TabStackParamList>()
 
 function TabStackNavigator(): JSX.Element {
   const renderTabBarBackground = useCallback(
-    () => <BlurView
-    // tint="light"
-    // control the intensity of the blur effect
-    blurAmount={20}
-    style={{
-      ...StyleSheet.absoluteFillObject,
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      // The overflow hidden is just to apply above radius.
-      overflow: "hidden",
-      // We can also apply background to tweak blur color
-      backgroundColor: "transparent",
-      // backgroundColor: "rgba(101, 92, 155, 0.4)",
-    }}
-  />,
+    () => (
+      <BlurView
+        // tint="light"
+        // control the intensity of the blur effect
+        blurAmount={20}
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          // The overflow hidden is just to apply above radius.
+          overflow: 'hidden',
+          // We can also apply background to tweak blur color
+          backgroundColor: 'transparent',
+          // backgroundColor: "rgba(101, 92, 155, 0.4)",
+        }}
+      />
+    ),
     [],
   )
 
-    const renderTabIcon = useCallback((props: TabIconProps) => {
+  const renderTabIcon = useCallback((props: TabIconProps) => {
     return <TabIcon {...props} />
   }, [])
 
   return (
     <TabStack.Navigator
-    screenOptions={{
-      headerShown: false,
-      tabBarStyle: {
-        // position absolute is required to extend the tab screens to also scroll behind the tab bar area,
-        // so the blur effect would only work if there is some content showing behind it.
-        position: "absolute",
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        shadowColor: "rgb(47, 64, 85)",
-        shadowOffset: { width: 0, height: -4 },
-        shadowOpacity: 0.12,
-        shadowRadius: 16,
-      },
-      tabBarBackground: renderTabBarBackground,
-    }}
-       >
-        <TabStack.Screen
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          // position absolute is required to extend the tab screens to also scroll behind the tab bar area,
+          // so the blur effect would only work if there is some content showing behind it.
+          position: 'absolute',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          shadowColor: 'rgb(47, 64, 85)',
+          shadowOffset: { width: 0, height: -4 },
+          shadowOpacity: 0.12,
+          shadowRadius: 16,
+        },
+        tabBarBackground: renderTabBarBackground,
+      }}
+    >
+      <TabStack.Screen
         name="Feed"
         component={FeedScreen}
         options={{
@@ -80,7 +82,7 @@ function TabStackNavigator(): JSX.Element {
           tabBarIcon: ({ focused }) => renderTabIcon({ focused, name: 'tickets' }),
         }}
       />
-       </TabStack.Navigator>
+    </TabStack.Navigator>
   )
 }
 
